33bd11d9f424b347d1c02cc1073eec3cba6fdfb3
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-14 13:58:01 +0200

    Add views, urls & templates for models

---
+++ todo/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from todo.views import TaskCreateView, TaskUpdateView, TaskDeleteView, TaskListView, TagListView, TagCreateView, \
+    TagUpdateView, TagDeleteView, change_task_status
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="task_list"),
+    path("tasks/create", TaskCreateView.as_view(), name="task_create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
+    path("tasks/<int:pk>/change_task_status/", change_task_status, name="change_status"),
+    path("tags/", TagListView.as_view(), name="tag_list"),
+    path("tags/create", TagCreateView.as_view(), name="tag_create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag_update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag_delete")
+]
+
+app_name = "todo"
