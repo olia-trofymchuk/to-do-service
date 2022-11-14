d8132ca61916d9e8e004c772d52904937e31a51e
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-14 13:53:54 +0200

    Add models to the project

---
+++ todo/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    is_completed = models.BooleanField()
+    tags = models.ManyToManyField(to=Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["is_completed", "created_at", "deadline"]
+
+    def __str__(self):
+        return f"{self.content} {self.created_at}"
